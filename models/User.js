b2e706d1ac8cbd99a0f29f8009e59e41b9937a4c
KaremAridi <karemaridi0@gmail.com>
2023-10-23 01:09:22 +0300

    .

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
+    admin: Boolean,
     userName: String,
     password: String,
     registeredCourses:{type: [mongoose.Schema.Types.ObjectId], ref:'Course', default: []},
