be5472bce0a9a687744fceb4446b82ccf808ba14
KaremAridi <karemaridi0@gmail.com>
2023-10-23 00:26:21 +0300

    adding stuff to backend

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/Louvre")
+    .then(() => {
+        console.log("connected to the db.");
+    }).catch(err => {
+        console.log("error in connecting. ERR: " + err);
+    });
+
+
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views','views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -9,3 +24,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
