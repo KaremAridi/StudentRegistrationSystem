150fbaff46996454025317f6324a0c1256e63c27
KaremAridi <karemaridi0@gmail.com>
2023-10-26 16:29:44 +0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 const path = require('path');
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://karem:1oYPZ9ksKDBed6k9@studentregistration.kz3uftj.mongodb.net/StudentRegistration")
+mongoose.connect("mongodb+srv://makramhasany:yONZyLEdIZm9tsC2@studentregistration.psxtf6w.mongodb.net/StudentRegistration")
     .then(() => {
         console.log("connected to the db.");
 
